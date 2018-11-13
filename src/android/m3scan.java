c2ec72bec17c553a66fdd0a9d9b2249232a28352
Thomas Hartmann <thartmann@north-tec.de>
2018-11-13 08:55:13 +0100

    setScanner

--- src/android/m3scan.java
+++ src/android/m3scan.java
@@ -55,8 +55,12 @@ public class m3scan extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         Log.d(getClass().getSimpleName(), action.toString());
         this.callbackContext = callbackContext;
-        Context context = this.cordova.getActivity().getApplicationContext();
-        if(action.equals("scanStart")) {
+        final Context context = this.cordova.getActivity().getApplicationContext();
+        if(action.equals("setScanner")) {
+            mBarcode.setScanner(args.getBoolean(0));
+            callbackContext.success(action);
+            return true;
+        } else if(action.equals("scanStart")) {
             mBarcode.scanStart();
             scanCallbackContext = callbackContext;
             return true;
